d8a9b8834c2d65abda11effb10def54a81c1187f
Carolina Prado <carolina.prado@live.com>
2025-04-25 08:41:47 -0300

    Componentes básicos consumindo dados do Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { DataProvider } from './context/DataContext';
 import Inicio from './pages/Index'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import GlobalStyles from './theme/GlobalStyles';
 
-const ContainerApp = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  margin: 0 auto;
-`;
+// const ContainerApp = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   min-height: 100vh;
+//   margin: 0 auto;
+// `;
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <ContainerApp>
-        <Header />
-        <Inicio />
-        <Footer />
-      </ContainerApp>
+      <DataProvider>
+        <GlobalStyles />
+        <div className="container">
+          <Header />
+          <Inicio />
+          <Footer />
+        </div>
+      </DataProvider>
+
     </>
   )
 }
